6f69a4917e3ae3657f58d8fd0d3a801780ba9c5e
Arghyadeep <arghyadeep.k@gmail.com>
2020-05-11 00:11:45 +0530

    Update messageStrings.js

--- lambda/messageStrings.js
+++ lambda/messageStrings.js
@@ -1,6 +1,6 @@
 module.exports = {
     en: {
-        //WelcomeMessage: `Hi! I'm a basic calculator.`,
+        welcome_message: null,
         boyles_law: `Boyle's law states that Pressure into Volume equals to a constant or P one V one equals to P two V two`,
         charles_law: `Charle's law states that Volume upon Temperature is a constant or V one by T one equals to V two by T two`
     }    
